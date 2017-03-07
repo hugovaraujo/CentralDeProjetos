3232420073df123e35dfbe856a901bf2883a8897
hugovaraujo <hugovaraujo@gmail.com>
2017-03-06 22:26:31 -0300

    Projeto Class, Constantes Class - 06/03

---
+++ CentralDeProjetos/src/com/ufcg/edu/centraldeprojetos/Constantes.java
@@ -0,0 +1,7 @@
+package com.ufcg.edu.centraldeprojetos;
+
+public interface Constantes {
+	public static final String FIM_DA_LINHA = "\n";
+	public statuc final String 
+	
+}
---
+++ CentralDeProjetos/src/com/ufcg/edu/centraldeprojetos/Projeto.java
@@ -0,0 +1,13 @@
+package com.ufcg.edu.centraldeprojetos;
+
+public abstract class Projeto {
+	
+	private String codigoProjeto;
+	private String nomeProjeto;
+	private int verbaProjeto;
+	private int valorBolsa;
+	
+	
+	
+	
+}
