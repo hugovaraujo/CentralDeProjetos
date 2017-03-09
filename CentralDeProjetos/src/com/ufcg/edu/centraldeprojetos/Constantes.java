d5d787ba9a5537d49c6a6e72f87039b2eb0a9e6c
hugovaraujo <hugovaraujo@gmail.com>
2017-03-08 22:36:40 -0300

    Merge Hugo to master

--- CentralDeProjetos/src/com/ufcg/edu/centraldeprojetos/Constantes.java
+++ CentralDeProjetos/src/com/ufcg/edu/centraldeprojetos/Constantes.java
@@ -1,5 +1,5 @@
 package com.ufcg.edu.centraldeprojetos;
 
 public interface Constantes {
-	public static final String FIM_DA_LINHA = "\n";
+	public static final String FIM_DA_LINHA = System.lineSeparator();
 }
