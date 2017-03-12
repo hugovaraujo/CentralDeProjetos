76baad2661e89117c60371136e33f1f4bd1173f0
Jailson Barros <jailson.junior@ccc.ufcg.edu.br>
2017-03-11 22:25:02 -0300

    Revert "Revert "Revert "Hierarquia de Exceções, Testes jUnit (Pessoa, PessoasCRUD e Verificador), us1_excetion e alguns ajustes"""
    This reverts commit 3840cb4fedb740d9c9efd39e9613dee50adcf09b.

--- CentralDeProjetos/src/factory/FactoryDePessoa.java
+++ CentralDeProjetos/src/factory/FactoryDePessoa.java
@@ -1,12 +1,11 @@
 package factory;
 
-import exceptions.CadastroException;
 import model.Pessoa;
 
 
 public class FactoryDePessoa {
 	
-	 public Pessoa criaPessoa(String cpf, String nome, String email) throws CadastroException{
+	 public Pessoa criaPessoa(String cpf, String nome, String email){
 			
 		 return new Pessoa(cpf, nome, email);
 		 
