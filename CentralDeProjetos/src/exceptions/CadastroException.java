a19f181153ed2e570adb7565e95063af5a54e1d1
Jailson Barros <jailson.junior@ccc.ufcg.edu.br>
2017-03-11 22:21:47 -0300

    Hierarquia de Exceções, Testes jUnit (Pessoa, PessoasCRUD e Verificador), us1_excetion e alguns ajustes

---
+++ CentralDeProjetos/src/exceptions/CadastroException.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+public class CadastroException extends ErroException  {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Construtor <b>CadastroInvalidoException</b>.
+	 * 
+	 * @param msg Mensagem relacionada ao objeto.
+	 * 
+	 */
+	public CadastroException(String msg){
+		super("no cadastro " + msg );
+
+	}
+}
--- CentralDeProjetos/src/factory/FactoryDePessoa.java
+++ CentralDeProjetos/src/factory/FactoryDePessoa.java
@@ -1,11 +1,12 @@
 package factory;
 
+import exceptions.CadastroException;
 import model.Pessoa;
 
 
 public class FactoryDePessoa {
 	
-	 public Pessoa criaPessoa(String cpf, String nome, String email){
+	 public Pessoa criaPessoa(String cpf, String nome, String email) throws CadastroException{
 			
 		 return new Pessoa(cpf, nome, email);
 		 
