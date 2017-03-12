ef7131cee7543b9327bcd2cd374f89b09fa52804
Jailson Barros <jailson.junior@ccc.ufcg.edu.br>
2017-03-11 22:28:23 -0300

    Revert "Revert "Revert "Revert "Hierarquia de Exceções, Testes jUnit (Pessoa, PessoasCRUD e Verificador), us1_excetion e alguns ajustes""""
    This reverts commit 76baad2661e89117c60371136e33f1f4bd1173f0.

---
+++ CentralDeProjetos/src/exceptions/FormatoInvalidoException.java
@@ -0,0 +1,21 @@
+package exceptions;
+
+public class FormatoInvalidoException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	/**
+	 * Construtor <b>StringInvalidaException</b>.
+	 * 
+	 * @param msg Nome do parametro que é invalido.
+	 * 
+	 */
+	public FormatoInvalidoException(String msg){
+		super(msg +" invalido");
+	}
+
+}
---
+++ CentralDeProjetos/src/exceptions/RemoverException.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+public class RemoverException extends ErroException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Construtor <b>CadastroInvalidoException</b>.
+	 * 
+	 * @param msg Mensagem relacionada ao objeto.
+	 * 
+	 */
+	public RemoverException(String msg){
+		super("ao remover " + msg );
+
+	}
+}
